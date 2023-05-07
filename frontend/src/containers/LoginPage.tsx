f2f3760a8f6a1a6f9f1386cf69a8764fac6e0c64
ikkio <kyomilo@outlook.com>
2023-05-07 16:59:07 +0800

    update: 更新了登陆检测。

--- frontend/src/containers/LoginPage.tsx
+++ frontend/src/containers/LoginPage.tsx
@@ -1,9 +1,18 @@
 // LoginPage.tsx
 import React from "react";
 import {LoginForm} from "../components/form/LoginForm";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage: React.FC = () => {
-    return (
+  const navigate = useNavigate();
+  const userString = localStorage.getItem("user");
+  if(userString){
+    alert("You are already logged in");
+    setTimeout(() => {
+      navigate('/');
+    }, 1000);
+  }
+  return (
         <div>
             <h1>Login Page</h1>
             <LoginForm  />
